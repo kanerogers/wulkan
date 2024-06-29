4fa302e55e922bbfd3ec150d25623e38280f3859
Kane Rogers-Wong <kane.m.rogers@gmail.com>
2024-06-30 09:06:07 +1000

    Physical device creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "vulkan/vulkan_core.h"
 #include <_types/_uint32_t.h>
 #include <exception>
+#include <sys/_types/_null.h>
+#include <vector>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -21,6 +23,7 @@ class HelloTriangleApplication {
     private:
         GLFWwindow* window;
         VkInstance instance;
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
         void initWindow() {
             glfwInit();
@@ -30,7 +33,6 @@ class HelloTriangleApplication {
             window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
         }
 
-
         void mainLoop() {
             while (!glfwWindowShouldClose(window)) {
                 glfwPollEvents();
@@ -39,6 +41,7 @@ class HelloTriangleApplication {
 
         void initVulkan() {
             createInstance();
+            createPhysicalDevice();
         }
 
         void createInstance() {
@@ -48,19 +51,57 @@ class HelloTriangleApplication {
             appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
             appInfo.pEngineName = "BEAST";
             appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.apiVersion = VK_API_VERSION_1_3;
+            appInfo.apiVersion = VK_API_VERSION_1_2;
 
             VkInstanceCreateInfo instanceCreateInfo{};
             instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
             instanceCreateInfo.pApplicationInfo = &appInfo;
 
+            std::vector<const char*> requiredExtensions;
+
             uint32_t glfwExtensionCount = 0;
             const char** glfwExtensions;
 
             glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+            for (uint32_t i=0; i < glfwExtensionCount; i++) {
+                requiredExtensions.emplace_back(glfwExtensions[i]);
+            }
+
+            requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+            instanceCreateInfo.enabledExtensionCount = requiredExtensions.size();
+            instanceCreateInfo.ppEnabledExtensionNames = requiredExtensions.data();
+            instanceCreateInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+            auto result = vkCreateInstance(&instanceCreateInfo, NULL, &instance);
+            if (result != VK_SUCCESS) {
+                throw std::runtime_error("failed to create instance!");
+            }
+        }
+
+        void createPhysicalDevice() {
+            uint32_t deviceCount = 0;
+
+            vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+            std::cout << "Found " << deviceCount << " devices" << std::endl;
+
+            std::vector<VkPhysicalDevice> devices(deviceCount);
+            vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+            for (const auto& device: devices) {
+                VkPhysicalDeviceProperties deviceProperties;
+                vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+                std::string deviceName(deviceProperties.deviceName);
+                std::cout << "Device name: " << deviceName << std::endl;
+            }
+
+            std::cout << "Done!" << std::endl;
         }
 
         void cleanup() {
+            vkDestroyInstance(instance, nullptr);
             glfwDestroyWindow(window);
             glfwTerminate();
         }
